4a6f43d7addd2b550153b911888c80be3c5c9c28
KentonB108 <61747696+KentonB108@users.noreply.github.com>
2020-03-09 23:05:14 -1200

    Tipo de datos

---
+++ Ejemplo netbeans/src/ejemplo/netbeans/EjemploNetbeans.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo.netbeans;
+
+/**
+ *
+ * @author Tecnilap
+ */
+public class EjemploNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    int numeroEntero = 0;
+double numeroDouble =0.0;
+char letra = 'a';
+String cadena = "Holaa";
+//solo puedes poner 1 valor por nombre
+boolean valor = true;
+boolean valores = false;
+//cambiar variable
+valores = true
+
+
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+//comentario
+
+
+      
+
